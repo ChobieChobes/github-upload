ca830af2e771008b6a9f2a7331fa9fe766a06660
Chobie <ChobieChobes@gmail.com>
2021-12-15 20:20:05 +0000

    Added letters feed generation

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var jsf = require('json-schema-faker')
 var fs = require('fs');
+var faker = require('faker');
+
+faker.locale = "en_GB";
 
 var childrenSchema = JSON.parse(fs.readFileSync('children.schema.json', 'utf8'));
+var lettersSchema = JSON.parse(fs.readFileSync('letters.schema.json', 'utf8'));
 
-jsf.extend('faker', () => require('faker'));
+jsf.extend('faker', () => faker);
+jsf.format('nl-address', () => `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.zipCodeByState()}`);
 
 var childrenData = jsf.generate(childrenSchema);
 
 console.log(JSON.stringify(childrenData))
 
-// Using the names generated in the above, we need now generate the santa letters
+var lettersData = jsf.generate(lettersSchema);
+
+// Overwrite letters names with names from children
+for (var i = 0; i < lettersData.length && i < childrenData.length; i++)
+{
+    lettersData[i].firstName = childrenData[i].firstName;
+    lettersData[i].familyName = childrenData[i].familyName;
+}
+
+console.log(JSON.stringify(lettersData))
+
+// TODO childrenData and lettersData now ready to be pushed to S3 bucket.
